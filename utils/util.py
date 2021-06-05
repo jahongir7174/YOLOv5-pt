0dd83a9fc8c04f2249228260ce0e2e60cb67ecf8
Jahongir <jahongir7174@gmail.com>
2021-06-05 12:42:50 +0900

    update

--- utils/util.py
+++ utils/util.py
@@ -230,10 +230,6 @@ def labels_to_class_weights(labels, nc=80):
     classes = labels[:, 0].astype(numpy.int)  # labels = [class xy-wh]
     weights = numpy.bincount(classes, minlength=nc)  # occurrences per class
 
-    # Prepend grid-point count (for uCE training)
-    # gpi = ((320 / 32 * np.array([1, 2, 4])) ** 2 * 3).sum()  # grid-points per image
-    # weights = np.hstack([gpi * len(labels)  - weights.sum() * 9, weights * 9]) ** 0.5  # prepend grid-points to start
-
     weights[weights == 0] = 1  # replace empty bins with 1
     weights = 1 / weights  # number of targets per class
     weights /= weights.sum()  # normalize
@@ -260,7 +256,7 @@ def wh2xy(x):
     return y
 
 
-def scale_coords(img1_shape, coords, img0_shape, ratio_pad=None):
+def scale_coordinates(img1_shape, coords, img0_shape, ratio_pad=None):
     # Rescale coords (xy_xy) from img1_shape to img0_shape
     if ratio_pad is None:  # calculate from img0_shape
         gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
@@ -272,11 +268,11 @@ def scale_coords(img1_shape, coords, img0_shape, ratio_pad=None):
     coords[:, [0, 2]] -= pad[0]  # x padding
     coords[:, [1, 3]] -= pad[1]  # y padding
     coords[:, :4] /= gain
-    clip_coords(coords, img0_shape)
+    clip_coordinates(coords, img0_shape)
     return coords
 
 
-def clip_coords(boxes, img_shape):
+def clip_coordinates(boxes, img_shape):
     # Clip bounding xy_xy bounding boxes to image shape (height, width)
     boxes[:, 0].clamp_(0, img_shape[1])  # x1
     boxes[:, 1].clamp_(0, img_shape[0])  # y1
